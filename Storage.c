80129bc578f49bd5ecce64a7127bc3e1656116b5
Sarv1120 <sarveshapathak1120@gmail.com>
2024-11-15 20:58:49 +0530

    Add files via upload

---
+++ Storage.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int Value = 11;  //Extern
+int Data;       //extern
+
+void Demo()
+{
+    int i = 10;     //auto
+    static int j = 20;   //static
+    register int k = 30;  //register
+}
+
+int main()
+{
+    int *p = NULL;
+
+    p = (int *)malloc(5 * sizeof(int));  //Dynamic memory
+
+    Demo();
+    
+    return 0;
+}
